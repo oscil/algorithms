9dba68ed141a334d733fb8d99fa04f85eab03696
Oscil <oscillatorium@gmail.com>
2019-01-22 18:36:44 +0300

    made some refactoring

--- binary_heap/main.py
+++ binary_heap/main.py
@@ -6,90 +6,88 @@ import math
 
 
 class BinaryHeap:
+    """Max binary heap"""
 
     def __init__(self):
-        self.arr = []
+        self.array = []
 
     def add(self, val):
-        self.arr.append(val)
-        self._up(len(self.arr)-1)
+        self.array.append(val)
+        self._up(len(self.array) - 1)
 
     def get_max(self):
-        self._swap(0, len(self.arr)-1)
-        out = self.arr.pop()
-        if len(self.arr) > 1:
+        self._swap(0, len(self.array) - 1)
+        out = self.array.pop()
+        if len(self.array) > 1:
             self._down(0)
         return out
 
     def _parent(self, index):
-        return self.arr[math.floor((index-1)/2)], math.floor((index-1)/2)
+        return self.array[math.floor((index - 1) / 2)], math.floor((index - 1) / 2)
 
-    def _lchild(self, index):
+    def _l_child(self, index):
         out_index = (index + 1)*2 - 1
-        if out_index < len(self.arr):
-            return self.arr[out_index], out_index
+        if out_index < len(self.array):
+            return self.array[out_index], out_index
         else:
-            return self.arr[index], index
+            return self.array[index], index
 
-    def _rchild(self, index):
+    def _r_child(self, index):
         out_index = (index + 1)*2
-        if out_index < len(self.arr):
-            return self.arr[out_index], out_index
+        if out_index < len(self.array):
+            return self.array[out_index], out_index
         else:
-            return self.arr[index], index
+            return self.array[index], index
 
     def _swap(self, index1, index2):
-        self.arr[index1], self.arr[index2] = self.arr[index2], self.arr[index1]
+        self.array[index1], self.array[index2] = self.array[index2], self.array[index1]
 
     def _up(self, index):
-        pval, pi = self._parent(index)
-        while pval < self.arr[index] and index > 0:
-            self._swap(index, pi)
-            index = pi
-            pval, pi = self._parent(index)
+        parent_value, parent_index = self._parent(index)
+        while parent_value < self.array[index] and index > 0:
+            self._swap(index, parent_index)
+            index = parent_index
+            parent_value, parent_index = self._parent(index)
 
     def _down(self, index):
         while True:
-            clval, cli = self._lchild(index)
-            crval, cri = self._rchild(index)
-            cval, ci = (crval, cri) if crval > clval else (clval, cli)
+            child_l_val, child_l_index = self._l_child(index)
+            child_r_val, child_r_index = self._r_child(index)
+            child_val, child_index = (child_r_val, child_r_index) \
+                                                   if child_r_val > child_l_val \
+                                                   else (child_l_val, child_l_index)
 
-            if self.arr[index] < cval:
-                self._swap(index, ci)
-                index = ci
+            if self.array[index] < child_val:
+                self._swap(index, child_index)
+                index = child_index
             else:
                 return
 
 
+if __name__ == '__main__':
+    array_size = 200
+    input_array = []
+    for i in range(array_size):
+        input_array.append(random.randint(0, 100))
+    random.shuffle(input_array)
 
+    bh = BinaryHeap()
 
+    print("Input array = " + str(input_array))
+    for v in input_array:
+        bh.add(v)
+    print("Array in binary heap= " + str(bh.array))
 
-
-size = 200
-
-arr = []
-for i in range(size):
-    arr.append(random.randint(0, 100))
-random.shuffle(arr)
-
-bh = BinaryHeap()
-
-print("ARR = " + str(arr))
-for v in arr:
-    bh.add(v)
-print("BH_ARR = " + str(bh.arr))
-
-
-while len(bh.arr):
-    prevarr = copy.copy(bh.arr)
-    m1 = max(bh.arr)
-    m2 = bh.get_max()
-    # print("after = " + str(bh.arr))
-    if m1 != m2:
-        print("ERROR!!!! m1 = " + str(m1) + " m2 = " + str(m2))
-        print("arr was: ")
-        print(prevarr)
-        break
+    while len(bh.array):
+        prev_bh_array = copy.copy(bh.array)
+        m1 = max(bh.array)
+        m2 = bh.get_max()
+        # check that got MAXIMUM is correct
+        if m1 != m2:
+            print("ERROR!!!! m1 = " + str(m1) + " m2 = " + str(m2))
+            print("arr was: ")
+            print(prev_bh_array)
+            break
 
 
 
